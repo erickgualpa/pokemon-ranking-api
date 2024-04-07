60031eaf3d40f15b997fa411d204e005e3e51a7c
Erick Gualpa <64378740+erickgualpa@users.noreply.github.com>
2024-04-07 16:51:31 +0200

    Update PokemonRankingApiApplication.java

--- src/main/java/org/egualpam/services/pokemon/pokemonrankingapi/infrastructure/PokemonRankingApiApplication.java
+++ src/main/java/org/egualpam/services/pokemon/pokemonrankingapi/infrastructure/PokemonRankingApiApplication.java
@@ -3,7 +3,12 @@ package org.egualpam.services.pokemon.pokemonrankingapi.infrastructure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.egualpam.services.pokemon.pokemonrankingapi.infrastructure.configuration",
+                "org.egualpam.services.pokemon.pokemonrankingapi.infrastructure.controllers",
+        }
+)
 public class PokemonRankingApiApplication {
 
     public static void main(String[] args) {
